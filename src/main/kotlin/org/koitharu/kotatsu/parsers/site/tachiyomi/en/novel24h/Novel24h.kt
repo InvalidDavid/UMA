3542f33aa866edf23639d5130e44f71bf82de519
InvalidDavid <118831950+InvalidDavid@users.noreply.github.com>
2026-06-23 23:08:29 +0200

    Add files via upload

---
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/tachiyomi/en/novel24h/Novel24h.kt
@@ -0,0 +1,10 @@
+package org.koitharu.kotatsu.parsers.site.tachiyomi.en.novel24h
+
+import org.koitharu.kotatsu.parsers.TachiyomiSource
+import org.koitharu.kotatsu.parsers.site.tachiyomi.parsers.Madara
+
+@TachiyomiSource("TWENTYFOURHNOVEL", "24HNovel", "en")
+class Novel24h : Madara("24HNovel", "https://24hnovel.com", "en") {
+    override val useLoadMoreRequest = LoadMoreStrategy.Never
+    override val useNewChapterEndpoint = true
+}
