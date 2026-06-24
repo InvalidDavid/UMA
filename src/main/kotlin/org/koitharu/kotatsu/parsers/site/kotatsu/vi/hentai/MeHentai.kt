bdbf733643ecfe16503b2554438dfe699d07035f
InvalidDavid <118831950+InvalidDavid@users.noreply.github.com>
2026-06-24 14:06:45 +0200

    im stupid

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/kotatsu/vi/hentai/MeHentai.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/kotatsu/vi/hentai/MeHentai.kt
@@ -6,6 +6,10 @@ import org.koitharu.kotatsu.parsers.model.ContentType
 import org.koitharu.kotatsu.parsers.model.MangaParserSource
 import org.koitharu.kotatsu.parsers.parsers.ManhwaZ
 
+// idk how to fix image cover url
+// they are using /cache-image/404
+// cant rly scrape them over html
+
 @MangaSourceParser("MEHENTAI", "MeHentai", "vi", ContentType.HENTAI)
 internal class MeHentai(context: MangaLoaderContext) :
     ManhwaZ(context, MangaParserSource.MEHENTAI, "mehentai.blog") {
