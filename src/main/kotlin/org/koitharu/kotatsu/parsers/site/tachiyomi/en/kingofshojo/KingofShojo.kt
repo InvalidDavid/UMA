24f769625d3ce5b1cafd11c09b108b7978058f24
InvalidDavid <118831950+InvalidDavid@users.noreply.github.com>
2026-06-24 16:32:29 +0200

    Delete src/main/kotlin/org/koitharu/kotatsu/parsers/site/tachiyomi directory

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/tachiyomi/en/kingofshojo/KingofShojo.kt
+++
@@ -1,7 +0,0 @@
-package org.koitharu.kotatsu.parsers.site.tachiyomi.en.kingofshojo
-
-import org.koitharu.kotatsu.parsers.site.tachiyomi.parsers.MangaThemesia
-import org.koitharu.kotatsu.parsers.TachiyomiSource
-
-@TachiyomiSource("KINGOFSHOJO", "King of Shojo", "en")
-class KingofShojo : MangaThemesia("King of Shojo", "https://kingofshojo.com", "en")
