552674b15661565a48249c81cac815c9b4e3c423
InvalidDavid <118831950+InvalidDavid@users.noreply.github.com>
2026-07-09 12:06:50 +0200

    simlify code

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/kotatsu/en/hentais/MangaDistrict.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/kotatsu/en/hentais/MangaDistrict.kt
@@ -6,62 +6,31 @@ import org.koitharu.kotatsu.parsers.MangaSourceParser
 import org.koitharu.kotatsu.parsers.model.ContentType
 import org.koitharu.kotatsu.parsers.model.Manga
 import org.koitharu.kotatsu.parsers.model.MangaChapter
-import org.koitharu.kotatsu.parsers.model.MangaListFilterOptions
 import org.koitharu.kotatsu.parsers.model.MangaParserSource
 import org.koitharu.kotatsu.parsers.model.MangaTag
+import org.koitharu.kotatsu.parsers.model.SortOrder
 import org.koitharu.kotatsu.parsers.parsers.MadaraParser
-import org.koitharu.kotatsu.parsers.site.tachiyomi.parsers.Madara.Genre
-import org.koitharu.kotatsu.parsers.util.attrAsRelativeUrl
-import org.koitharu.kotatsu.parsers.util.generateUid
-import org.koitharu.kotatsu.parsers.util.mapNotNullToSet
-import org.koitharu.kotatsu.parsers.util.mapToSet
-import org.koitharu.kotatsu.parsers.util.parseHtml
-import org.koitharu.kotatsu.parsers.util.selectFirstOrThrow
-import org.koitharu.kotatsu.parsers.util.toAbsoluteUrl
-import org.koitharu.kotatsu.parsers.util.toTitleCase
+import org.koitharu.kotatsu.parsers.util.*
 import java.text.SimpleDateFormat
+import java.util.EnumSet
 
 @MangaSourceParser("MANGADISTRICT", "MangaDistrict", "en", ContentType.HENTAI)
-internal class MangaDistrict(context: MangaLoaderContext):
-    MadaraParser(context, MangaParserSource.MANGADISTRICT, "mangadistrict.com") {
+internal class MangaDistrict(context: MangaLoaderContext) :
+    MadaraParser(context, MangaParserSource.MANGADISTRICT, "mangadistrict.com", pageSize = 30) {
 
+    override val tagPrefix = "publication-genre/"
     override val withoutAjax: Boolean = true
     override val datePattern: String = "MMMM d, yyyy"
     override val stylePage: String = "?style=list"
 
-    override fun parseMangaList(doc: Document): List<Manga> {
-        return super.parseMangaList(doc).map { manga ->
-            manga.copy(
-                title = manga.title.cleanTitleIfNeeded()
-            )
-        }
-    }
-
-    private fun String.cleanTitleIfNeeded(): String {
-        return this
-            .replace(Regex("""\s*\(.*?\)\s*"""), "") // remove brackets
-            .replace(Regex("""\s*\[.*?]\s*"""), "")
-            .trim()
-    }
-
-    override suspend fun fetchAvailableTags(): Set<MangaTag> {
-        val doc = webClient.httpGet("https://$domain/series/").parseHtml()
-        val elements = doc.select("header ul.second-menu li a, div.genres_wrap ul li a")
-
-        return elements.mapNotNullToSet { a ->
-            val href = a.attr("href")
-                .removeSuffix("/")
-                .substringAfterLast(tagPrefix, "")
-
-            if (href.isBlank()) return@mapNotNullToSet null
-
-            MangaTag(
-                key = href,
-                title = a.text().toTitleCase(),
-                source = source,
-            )
-        }
-    }
+    override val availableSortOrders: Set<SortOrder> = EnumSet.of(
+        SortOrder.RELEVANCE,
+        SortOrder.NEWEST,
+        SortOrder.ALPHABETICAL,
+        SortOrder.RATING,
+        SortOrder.POPULARITY,
+        SortOrder.UPDATED,
+    )
 
     override suspend fun getChapters(manga: Manga, doc: Document): List<MangaChapter> {
         val dateFormat = SimpleDateFormat(datePattern, sourceLocale)
@@ -112,132 +81,26 @@ internal class MangaDistrict(context: MangaLoaderContext):
             ?.toFloatOrNull()
     }
 
-    override suspend fun getDetails(manga: Manga): Manga {
-        val doc = webClient.httpGet(manga.url.toAbsoluteUrl(domain)).parseHtml()
+    override suspend fun fetchAvailableTags(): Set<MangaTag> {
+        val doc = webClient.httpGet("https://$domain/series/").parseHtml()
+        val elements = doc.select("header ul.second-menu li a, div.genres_wrap ul li a")
 
-        val base = super.getDetails(manga)
+        return elements.mapNotNullToSet { a ->
+            val href = a.attr("href")
+                .removeSuffix("/")
+                .substringAfterLast(tagPrefix, "")
 
-        return base.copy(
-            title = doc.selectFirst("h1")?.text() ?: base.title,
-            tags = doc.select("div.genres-content a").mapToSet { a ->
-                MangaTag(
-                    key = a.attr("href")
-                        .removeSuffix("/")
-                        .substringAfterLast('/'),
-                    title = a.text().toTitleCase(),
-                    source = source,
-                )
-            }
-        )
+            if (href.isBlank()) return@mapNotNullToSet null
+
+            MangaTag(
+                key = href,
+                title = a.text()
+                    .replace(Regex("""\s*\(\d+\)"""), "")
+                    .replace(Regex("""[^\x20-\x7E]"""), "")
+                    .trim()
+                    .toTitleCase(),
+                source = source,
+            )
+        }
     }
-
-    private var genresList = listOf(
-        Genre("3D", "3d"),
-        Genre("Action", "action"),
-        Genre("Adapted to Anime", "adapted-to-anime"),
-        Genre("Adventure", "adventure"),
-        Genre("Aliens", "aliens"),
-        Genre("Animal Characteristics", "animal-characteristics"),
-        Genre("Based on Another Work", "based-on-another-work"),
-        Genre("BL", "bl"),
-        Genre("BL Uncensored", "bl-uncensored"),
-        Genre("Borderline H", "borderline-h"),
-        Genre("Cohabitation", "cohabitation"),
-        Genre("Collection of Stories", "collection-of-stories"),
-        Genre("Comedy", "comedy"),
-        Genre("Comics", "comics"),
-        Genre("Cooking", "cooking"),
-        Genre("Coworkers", "coworkers"),
-        Genre("Crime", "crime"),
-        Genre("Crossdressing", "crossdressing"),
-        Genre("Delinquents", "delinquents"),
-        Genre("Demons", "demons"),
-        Genre("Detectives", "detectives"),
-        Genre("Doujinshi", "doujinshi"),
-        Genre("Drama", "drama"),
-        Genre("Ecchi", "ecchi"),
-        Genre("Explicit Sex", "explicit-sex"),
-        Genre("Fantasy", "fantasy"),
-        Genre("Fetish", "fetish"),
-        Genre("Full Color", "full-color"),
-        Genre("Gender Bender", "gender-bender"),
-        Genre("Ghosts", "ghosts"),
-        Genre("GL", "gl"),
-        Genre("Gyaru", "gyaru"),
-        Genre("Harem", "harem"),
-        Genre("Historical", "historical"),
-        Genre("Horror", "horror"),
-        Genre("Incest", "incest"),
-        Genre("Isekai", "isekai"),
-        Genre("Japanese Webtoons", "japanese-webtoons"),
-        Genre("Josei", "josei"),
-        Genre("Light Novels", "light-novels"),
-        Genre("Mafia", "mafia"),
-        Genre("Magic", "magic"),
-        Genre("Magical Girl", "magical-girl"),
-        Genre("Manhua", "manhua"),
-        Genre("Manhwa", "manhwa"),
-        Genre("Martial Arts", "martial-arts"),
-        Genre("Mature Romance", "mature-romance"),
-        Genre("Mecha", "mecha"),
-        Genre("Medical", "medical"),
-        Genre("Military", "military"),
-        Genre("Monster Girls", "monster-girls"),
-        Genre("Monsters", "monsters"),
-        Genre("Music", "music"),
-        Genre("Mystery", "mystery"),
-        Genre("Ninja", "ninja"),
-        Genre("Nudity", "nudity"),
-        Genre("One Shot", "one-shot"),
-        Genre("Person in a Strange World", "person-in-a-strange-world"),
-        Genre("Police", "police"),
-        Genre("Psychological", "psychological"),
-        Genre("Reincarnation", "reincarnation"),
-        Genre("Reverse Harem", "reverse-harem"),
-        Genre("Romance", "romance"),
-        Genre("Salaryman", "salaryman"),
-        Genre("Samurai", "samurai"),
-        Genre("School Life", "school-life"),
-        Genre("Sci Fi", "sci-fi"),
-        Genre("Seinen", "seinen"),
-        Genre("Sexual Abuse", "sexual-abuse"),
-        Genre("Sexual Content", "sexual-content"),
-        Genre("Shoujo", "shoujo"),
-        Genre("Shoujo-ai", "shoujo-ai"),
-        Genre("Shounen", "shounen"),
-        Genre("Shounen-ai", "shounen-ai"),
-        Genre("Siblings", "siblings"),
-        Genre("Slice of Life", "slice-of-life"),
-        Genre("Smut", "smut"),
-        Genre("Sports", "sports"),
-        Genre("Summoned Into Another World", "summoned-into-another-world"),
-        Genre("Superheroes", "superheroes"),
-        Genre("Supernatural", "supernatural"),
-        Genre("Survival", "survival"),
-        Genre("Thriller", "thriller"),
-        Genre("Time Travel", "time-travel"),
-        Genre("Transfer Students", "transfer-students"),
-        Genre("Uncensored", "uncensored"),
-        Genre("Vampires", "vampires"),
-        Genre("Violence", "violence"),
-        Genre("Virtual Reality", "virtual-reality"),
-        Genre("Web Novels", "web-novels"),
-        Genre("Webtoons", "webtoons"),
-        Genre("Western", "western"),
-        Genre("Work Life", "work-life"),
-        Genre("Yaoi", "yaoi"),
-        Genre("Yuri", "yuri"),
-        Genre("Zombies", "zombies"),
-    )
-
-    override suspend fun getFilterOptions(): MangaListFilterOptions =
-        MangaListFilterOptions(
-            availableTags = genresList.mapToSet { genre ->
-                MangaTag(
-                    key = genre.id,
-                    title = genre.name,
-                    source = source
-                )
-            }
-        )
 }
