203f2a9a629642e28672846f0c5011d9f0b48b6f
InvalidDavid <118831950+InvalidDavid@users.noreply.github.com>
2026-07-14 13:37:39 +0200

    Add @Broken annotation to Hiperdex class

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/kotatsu/en/hentais/Hiperdex.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/kotatsu/en/hentais/Hiperdex.kt
@@ -1,12 +1,14 @@
 package org.koitharu.kotatsu.parsers.site.kotatsu.en.hentais
 
 import okhttp3.Headers
+import org.koitharu.kotatsu.parsers.Broken
 import org.koitharu.kotatsu.parsers.MangaLoaderContext
 import org.koitharu.kotatsu.parsers.MangaSourceParser
 import org.koitharu.kotatsu.parsers.model.ContentType
 import org.koitharu.kotatsu.parsers.model.MangaParserSource
 import org.koitharu.kotatsu.parsers.parsers.HiperParser
 
+@Broken
 @MangaSourceParser("HIPERDEX", "Hiperdex", "en", ContentType.HENTAI)
 internal class Hiperdex(context: MangaLoaderContext) :
     HiperParser(context, MangaParserSource.HIPERDEX, "hiperdex.com") {
