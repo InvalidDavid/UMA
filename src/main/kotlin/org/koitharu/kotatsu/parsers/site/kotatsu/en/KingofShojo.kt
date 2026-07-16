05dc12ff46b7bb91305b6b0fed7f20a0d4459f1c
InvalidDavid <118831950+InvalidDavid@users.noreply.github.com>
2026-07-16 12:27:32 +0200

    Fix formatting in KingofShojo.kt

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/kotatsu/en/KingofShojo.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/kotatsu/en/KingofShojo.kt
@@ -5,7 +5,6 @@ import org.koitharu.kotatsu.parsers.MangaSourceParser
 import org.koitharu.kotatsu.parsers.model.MangaParserSource
 import org.koitharu.kotatsu.parsers.parsers.MangaThemesia
 
-
 @MangaSourceParser("KINGOFSHOJO", "King of Shojo", "en")
 internal class KingofShojo(context: MangaLoaderContext) :
     MangaThemesia(context, MangaParserSource.KINGOFSHOJO, "kingofshojo.com")
