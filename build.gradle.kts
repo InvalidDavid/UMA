e96e5caa05cf2b249682a99fa7abd40a47a7e148
Draken <131387159+dragonx943@users.noreply.github.com>
2026-07-19 03:18:58 +0700

    Migrate to new Tsuki library + new parsers structure (#1)
    * Migrate to new Tsuki library + new parsers structure
    
    * Refactor code, clean up
    
    * Bump version, clean up

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,7 +34,7 @@ tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach 
             "-opt-in=kotlin.RequiresOptIn",
             "-opt-in=kotlin.contracts.ExperimentalContracts",
             "-opt-in=kotlinx.coroutines.ExperimentalCoroutinesApi",
-            "-opt-in=org.koitharu.kotatsu.parsers.InternalParsersApi",
+            "-opt-in=tsuki.InternalParsersApi",
         )
     }
 }
@@ -54,16 +54,13 @@ publishing {
 
 dependencies {
     implementation(libs.kotlinx.coroutines.core)
-	implementation(libs.kotlinx.serialization.json)
     implementation(libs.okhttp)
     implementation(libs.okio)
     implementation(libs.json)
     implementation(libs.androidx.collection)
 
-	api(libs.core.parsers)
+	api(libs.tsuki)
     api(libs.jsoup)
-
-    compileOnly(libs.android.stubs)
 
     ksp(project(":plugins-ksp"))
 
@@ -76,14 +73,18 @@ dependencies {
 }
 
 tasks.register("buildJar") {
+    description = "Build all sources to a JAR file"
     dependsOn("dexJar")
 }
 
 tasks.register<DexPluginTask>("dexJar") {
+    description = "Dex classes after build"
     dependsOn(tasks.jar)
     inputJar.set(tasks.jar.flatMap { it.archiveFile })
     outputJar.set(layout.projectDirectory.file("build/libs/vn.jar"))
     classpath.from(configurations.runtimeClasspath)
 }
 
-tasks.register<ReportGenerateTask>("generateTestsReport")
+tasks.register<ReportGenerateTask>("generateTestsReport") {
+    description = "Generate a HTML file to get tests report"
+}
