853e201b34dce9aea49e99c3bc1a5d973a07228b
InvalidDavid <118831950+InvalidDavid@users.noreply.github.com>
2026-07-23 23:55:24 +0200

    Update MangaSusuku.kt

--- src/main/kotlin/tsuki/site/id/hentais/MangaSusuku.kt
+++ src/main/kotlin/tsuki/site/id/hentais/MangaSusuku.kt
@@ -2,18 +2,17 @@ package tsuki.site.id.hentais
 
 import tsuki.MangaLoaderContext
 import tsuki.MangaSourceParser
-import tsuki.parsers.MangaReaderParser
+import tsuki.parsers.MangaThemesia
 
 import tsuki.model.ContentType
 import tsuki.model.MangaParserSource
 
+import java.text.SimpleDateFormat
 import java.util.Locale
 
 @MangaSourceParser("MANGASUSUKU", "MangaSusuku", "id", ContentType.HENTAI)
 internal class MangaSusuku(context: MangaLoaderContext) :
-    MangaReaderParser(context, MangaParserSource.MANGASUSUKU, "mangasusuku.com", pageSize = 20, searchPageSize = 20) {
-    override val listUrl = "/komik"
-    override val datePattern = "MMM d, yyyy"
-    override val sourceLocale: Locale = Locale.ENGLISH
-    override val isNetShieldProtected = true
+    MangaThemesia(context, MangaParserSource.MANGASUSUKU, "mangasusuku.com") {
+    override val mangaDirectory = "komik"
+    override val dateFormat = SimpleDateFormat("MMM d, yyyy", Locale.ENGLISH)
 }
