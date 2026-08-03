ebadb56392e98de0ad9365d636b4278fa5c4c5ec
InvalidDavid <118831950+InvalidDavid@users.noreply.github.com>
2026-08-03 22:38:35 +0200

    Update Multporn.kt

--- src/main/kotlin/tsuki/site/all/nsfw/Multporn.kt
+++ src/main/kotlin/tsuki/site/all/nsfw/Multporn.kt
@@ -6,8 +6,30 @@ import tsuki.config.ConfigKey
 import tsuki.core.PagedMangaParser
 import tsuki.network.CommonHeaders
 
-import tsuki.model.*
-import tsuki.util.*
+import tsuki.model.ContentRating
+import tsuki.model.ContentType
+import tsuki.model.Manga
+import tsuki.model.MangaChapter
+import tsuki.model.MangaListFilter
+import tsuki.model.MangaListFilterCapabilities
+import tsuki.model.MangaListFilterOptions
+import tsuki.model.MangaPage
+import tsuki.model.MangaParserSource
+import tsuki.model.MangaState
+import tsuki.model.MangaTag
+import tsuki.model.RATING_UNKNOWN
+import tsuki.model.SortOrder
+
+import tsuki.util.attrAsAbsoluteUrl
+import tsuki.util.attrAsRelativeUrl
+import tsuki.util.generateUid
+import tsuki.util.oneOrThrowIfMany
+import tsuki.util.parseHtml
+import tsuki.util.requireSrc
+import tsuki.util.selectFirstOrThrow
+import tsuki.util.splitByWhitespace
+import tsuki.util.toAbsoluteUrl
+import tsuki.util.urlEncoded
 
 import okhttp3.Headers
 import org.jsoup.nodes.Document
