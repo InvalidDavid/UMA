0cf7cbec57782b9ab0450363dbd3f544343655a5
InvalidDavid <118831950+InvalidDavid@users.noreply.github.com>
2026-08-06 23:56:56 +0200

    Update Manhwa210.kt

--- src/main/kotlin/tsuki/site/en/nsfw/Manhwa210.kt
+++ src/main/kotlin/tsuki/site/en/nsfw/Manhwa210.kt
@@ -5,8 +5,32 @@ import tsuki.MangaSourceParser
 import tsuki.config.ConfigKey
 import tsuki.core.PagedMangaParser
 
-import tsuki.model.*
-import tsuki.util.*
+import tsuki.model.ContentRating
+import tsuki.model.ContentType
+import tsuki.model.Manga
+import tsuki.model.MangaChapter
+import tsuki.model.MangaListFilter
+import tsuki.model.MangaListFilterCapabilities
+import tsuki.model.MangaListFilterOptions
+import tsuki.model.MangaPage
+import tsuki.model.MangaParserSource
+import tsuki.model.MangaState
+import tsuki.model.MangaTag
+import tsuki.model.RATING_UNKNOWN
+import tsuki.model.SortOrder
+
+import tsuki.util.attrAsRelativeUrl
+import tsuki.util.attrOrNull
+import tsuki.util.generateUid
+import tsuki.util.mapToSet
+import tsuki.util.parseFailed
+import tsuki.util.parseHtml
+import tsuki.util.removeSuffix
+import tsuki.util.requireSrc
+import tsuki.util.selectLast
+import tsuki.util.textOrNull
+import tsuki.util.toAbsoluteUrl
+import tsuki.util.urlEncoded
 
 import kotlinx.coroutines.sync.Mutex
 import kotlinx.coroutines.sync.withLock
