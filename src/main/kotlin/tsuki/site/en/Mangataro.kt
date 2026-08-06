345539f580d35c348a316771ae2c6bb67ef1b4fd
InvalidDavid <118831950+InvalidDavid@users.noreply.github.com>
2026-08-07 01:10:25 +0200

    Update Mangataro.kt

--- src/main/kotlin/tsuki/site/en/Mangataro.kt
+++ src/main/kotlin/tsuki/site/en/Mangataro.kt
@@ -6,8 +6,27 @@ import tsuki.config.ConfigKey
 import tsuki.core.PagedMangaParser
 import tsuki.exception.ParseException
 
-import tsuki.model.*
-import tsuki.util.*
+import tsuki.model.ContentType
+import tsuki.model.Manga
+import tsuki.model.MangaChapter
+import tsuki.model.MangaListFilter
+import tsuki.model.MangaListFilterCapabilities
+import tsuki.model.MangaListFilterOptions
+import tsuki.model.MangaPage
+import tsuki.model.MangaParserSource
+import tsuki.model.MangaState
+import tsuki.model.MangaTag
+import tsuki.model.RATING_UNKNOWN
+import tsuki.model.SortOrder
+
+import tsuki.util.attrAsAbsoluteUrlOrNull
+import tsuki.util.generateUid
+import tsuki.util.mapNotNullToSet
+import tsuki.util.nullIfEmpty
+import tsuki.util.parseHtml
+import tsuki.util.parseRaw
+import tsuki.util.toAbsoluteUrl
+import tsuki.util.toRelativeUrl
 
 import okhttp3.Headers
 import org.json.JSONArray
@@ -25,6 +44,7 @@ import java.util.EnumSet
 import java.util.HashSet
 import java.util.LinkedHashSet
 import java.util.Locale
+import kotlin.collections.isNotEmpty
 
 @MangaSourceParser("MANGATAROORG", "Mangataro", "en", ContentType.MANGA)
 internal class Mangataro(context: MangaLoaderContext) :
@@ -58,8 +78,16 @@ internal class Mangataro(context: MangaLoaderContext) :
 
     override suspend fun getFilterOptions() = MangaListFilterOptions(
         availableTags = genreTags,
-        availableStates = EnumSet.of(MangaState.ONGOING, MangaState.FINISHED),
-        availableContentTypes = EnumSet.of(ContentType.MANGA, ContentType.MANHWA, ContentType.MANHUA, ContentType.OTHER),
+        availableStates = EnumSet.of(
+            MangaState.ONGOING, 
+            MangaState.FINISHED
+        ),
+        availableContentTypes = EnumSet.of(
+            ContentType.MANGA,
+            ContentType.MANHWA,
+            ContentType.MANHUA,
+            ContentType.OTHER
+        ),
     )
 
 
@@ -182,6 +210,7 @@ internal class Mangataro(context: MangaLoaderContext) :
         return result
     }
 
+    @Suppress("SameParameterValue")
     private fun buildChapterUrl(
         mangaId: String,
         offset: Int,
