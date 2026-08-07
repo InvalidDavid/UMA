a9feb706dc4c3a4fce172c133a1dc39651bf0242
InvalidDavid <118831950+InvalidDavid@users.noreply.github.com>
2026-08-07 13:17:21 +0200

    Refactor Manhwa18CC class and update imports

--- src/main/kotlin/tsuki/site/en/nsfw/Manhwa18CC.kt
+++ src/main/kotlin/tsuki/site/en/nsfw/Manhwa18CC.kt
@@ -94,15 +94,14 @@ internal class Manhwa18CC(context: MangaLoaderContext) :
     )
 
     override suspend fun getListPage(page: Int, order: SortOrder, filter: MangaListFilter): List<Manga> {
-        val query = filter.query
         val url = buildString {
             append("https://")
             append(domain)
 
             when {
-                !query.isNullOrEmpty() -> {
+                !filter.query.isNullOrEmpty() -> {
                     append("/search?q=")
-                    append(query.urlEncoded())
+                    append(filter.query.urlEncoded())
                     append("&page=")
                     append(page.toString())
                 }
