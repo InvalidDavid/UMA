82d02539453742f8a76267d3aa395e332b40cbc7
InvalidDavid <118831950+InvalidDavid@users.noreply.github.com>
2026-08-07 13:16:46 +0200

    Update LikeMangaParser.kt

--- src/main/kotlin/tsuki/parsers/LikeMangaParser.kt
+++ src/main/kotlin/tsuki/parsers/LikeMangaParser.kt
@@ -85,17 +85,16 @@ internal abstract class LikeMangaParser(
     )
 
     override suspend fun getListPage(page: Int, order: SortOrder, filter: MangaListFilter): List<Manga> {
-        val query = filter.query
         val url = buildString {
             append("https://")
             append(domain)
             append("/?act=search")
 
-            query?.let {
+            filter.query?.let {
                 append("&f")
                 append("[keyword]".urlEncoded())
                 append("=")
-                append(query.urlEncoded())
+                append(filter.query.urlEncoded())
             }
 
             append("&f")
