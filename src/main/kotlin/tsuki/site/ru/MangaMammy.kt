a88cd2cf9de1077536d6613ac50f4f982108f70d
InvalidDavid <118831950+InvalidDavid@users.noreply.github.com>
2026-09-06 18:26:30 +0200

    Update MangaMammy.kt

--- src/main/kotlin/tsuki/site/ru/MangaMammy.kt
+++ src/main/kotlin/tsuki/site/ru/MangaMammy.kt
@@ -2,12 +2,13 @@ package tsuki.site.ru
 
 import tsuki.MangaLoaderContext
 import tsuki.MangaSourceParser
-import tsuki.model.MangaParserSource
 import tsuki.parsers.MadaraParser
+
+import tsuki.model.MangaParserSource
 
 @MangaSourceParser("MANGAMAMMY", "Nimanga", "ru")
 internal class MangaMammy(context: MangaLoaderContext) :
-	MadaraParser(context, MangaParserSource.MANGAMAMMY, "p.nimanga.com") {
-	override val datePattern = "dd.MM.yyyy"
-	override val postReq = true
+    MadaraParser(context, MangaParserSource.MANGAMAMMY, "p.nimanga.com") {
+    override val datePattern = "dd.MM.yyyy"
+    override val postReq = true
 }
