54e03b1bf0986799f161a2325e3a618ce83504fc
InvalidDavid <118831950+InvalidDavid@users.noreply.github.com>
2026-09-06 18:12:54 +0200

    Update and rename SlashLibParser.kt to SlashLib.kt

--- src/main/kotlin/tsuki/site/ru/SlashLib.kt
+++ src/main/kotlin/tsuki/site/ru/SlashLib.kt
@@ -3,12 +3,9 @@ package tsuki.site.ru
 import tsuki.parsers.LibSocialParser
 import tsuki.MangaLoaderContext
 import tsuki.MangaSourceParser
+
 import tsuki.model.MangaParserSource
 
 @MangaSourceParser("YAOILIB", "SlashLib", "ru")
-internal class SlashLibParser(context: MangaLoaderContext) : LibSocialParser(
-	context = context,
-	source = MangaParserSource.YAOILIB,
-	siteId = 2,
-	siteDomains = arrayOf("v2.slashlib.me"),
-)
+internal class SlashLibParser(context: MangaLoaderContext) : 
+    LibSocialParser(context, MangaParserSource.YAOILIB, 2, "v2.slashlib.me")
