6e9c5b1dee04d215f7253d66e1c415eedddd1d81
InvalidDavid <118831950+InvalidDavid@users.noreply.github.com>
2026-09-13 10:08:15 +0200

    Update ToonTopIO.kt

--- src/main/kotlin/tsuki/site/en/nsfw/ToonTopIO.kt
+++ src/main/kotlin/tsuki/site/en/nsfw/ToonTopIO.kt
@@ -2,7 +2,6 @@ package tsuki.site.en.nsfw
 
 import tsuki.MangaLoaderContext
 import tsuki.MangaSourceParser
-import tsuki.config.ConfigKey
 import tsuki.parsers.MangaKParser
 
 import tsuki.model.ContentType
@@ -10,6 +9,4 @@ import tsuki.model.MangaParserSource
 
 @MangaSourceParser("TOONTOPIO", "ToonTop.io", "en", ContentType.HENTAI)
 internal class ToonTopIO(context: MangaLoaderContext) :
-    MangaKParser(context, MangaParserSource.TOONTOPIO) {
-    override val configKeyDomain = ConfigKey.Domain("toontop.io")
-}
+    MangaKParser(context, MangaParserSource.TOONTOPIO, "toontop.io")
